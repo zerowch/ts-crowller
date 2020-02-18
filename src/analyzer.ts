0192a98dc08868377cf5048a9aeb28ca68c5e3b9
zerowch <zerowch@qq.com>
2020-02-18 11:42:36 +0800

    course3-5

---
+++ src/analyzer.ts
@@ -0,0 +1,77 @@
+/*
+ * @Author: Zero W
+ * @Date: 2020-02-18 09:49:13
+ * @Description: 
+ * @LastEditTime : 2020-02-18 11:41:40
+ * @LastEditors  : Zero W
+ * @-<-zw->-
+ */
+
+import fs from 'fs';
+import cheerio from 'cheerio';
+import { Analyzer } from './crowller'
+
+interface Course {
+  title: string;
+  count: number;
+}
+
+interface CourseResult {
+  time: number;
+  data: Course[];
+}
+
+interface Content {
+  [propName: number]: Course[];
+}
+
+export default class analyzer implements Analyzer {
+  constructor() {}
+  
+  private getCourseInfo (html: string) {
+    const $ = cheerio.load(html)
+    const courseItems = $('.course-item');
+    // console.log(typeof courseItems)
+    let course: Course[] = []
+    courseItems.map((index, element) => {
+      const desc = $(element).find('.course-desc');
+      const title = desc.eq(0).text();
+      const count = parseInt(desc.eq(1).text().split('：')[1])
+
+
+      course.push({
+        title,
+        count
+      })
+    })
+
+    return {
+      time: new Date().getTime(),
+      data: course
+    }
+  }
+
+  private generateJsonContent (courseInfo: CourseResult, filePath: string) {
+    // const filePath = this.FilePath
+    let fileContent: Content = {};
+    if (fs.existsSync(filePath)) {
+      const readContent = fs.readFileSync(filePath, 'utf-8')
+      try {
+        fileContent = JSON.parse(readContent);
+        fileContent[courseInfo.time] = courseInfo.data;
+      } catch (error) {
+        console.log('原文件json数据格式有误')
+        fileContent = {}
+      }
+    }
+    // console.log(fileContent)
+    return fileContent
+  }
+
+  public analyze (html: string, filePath: string) {
+    const courseInfo = this.getCourseInfo(html)
+    const fileContent = this.generateJsonContent(courseInfo, filePath)
+
+    return JSON.stringify(fileContent)
+  }
+}
--- src/crowller.ts
+++ src/crowller.ts
@@ -2,59 +2,23 @@
  * @Author: Zero W
  * @Date: 2020-02-13 14:56:06
  * @Description: crowller
- * @LastEditTime : 2020-02-14 17:26:45
+ * @LastEditTime : 2020-02-18 11:36:03
  * @LastEditors  : Zero W
  * @-<-zw->-
  */
 import superagent from 'superagent';
-import cheerio from 'cheerio';
-
 import fs from 'fs';
 import path from 'path';
+import analyzer from './analyzer';
 
-interface Course {
-  title: string;
-  count: number;
-}
-
-interface CourseResult {
-  time: number;
-  data: Course[];
-}
-
-interface Content {
-  [propName: number]: Course[];
+export interface Analyzer {
+  analyze: (html: string, filePath: string) => string
 }
 
 class Crowller {
-  private secret = 'secretKey';
-  private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`;
   // private rawHtml = '';
   // public crowData: unknown
-  FilePath = path.resolve(__dirname, '../data/course.json')
-
-  private getCourseInfo (html: string) {
-    const $ = cheerio.load(html)
-    const courseItems = $('.course-item');
-    // console.log(typeof courseItems)
-    let course: Course[] = []
-    courseItems.map((index, element) => {
-      const desc = $(element).find('.course-desc');
-      const title = desc.eq(0).text();
-      const count = parseInt(desc.eq(1).text().split('：')[1])
-
-
-      course.push({
-        title,
-        count
-      })
-    })
-
-    return {
-      time: new Date().getTime(),
-      data: course
-    }
-  }
+  private FilePath = path.resolve(__dirname, '../data/course.json')
 
   async getRawHtml () {
     const result = await superagent.get(this.url);
@@ -62,39 +26,29 @@ class Crowller {
     // this.getHtmlData(this.rawHtml)
   }
 
-  async generateJsonContent (courseInfo: CourseResult) {
+  writeFile (content: string) {
     const filePath = this.FilePath
-    let fileContent: Content = {};
-    if (fs.existsSync(filePath)) {
-      const readContent = fs.readFileSync(filePath, 'utf-8')
-      try {
-        fileContent = JSON.parse(readContent);
-      } catch (error) {
-        console.log('原文件json数据格式有误')
-      }
-    }
-    fileContent[courseInfo.time] = courseInfo.data;
-    // console.log(fileContent)
-    return fileContent
+    fs.writeFileSync(filePath, content)
   }
 
   async initSpiderProcess () {
-    const filePath = this.FilePath
+    
     const html = await this.getRawHtml()
-    const courseInfo = this.getCourseInfo(html)
-
-    const fileContent = await this.generateJsonContent(courseInfo)
-
-    fs.writeFileSync(filePath, JSON.stringify(fileContent));
+    const fileContent = this.analyzer.analyze(html, this.FilePath)
+    this.writeFile(fileContent)
   }
 
-  constructor () {
+  constructor (private url: string, private analyzer: Analyzer) {
     console.log('crowller constructor');
     this.initSpiderProcess();
   }
 }
 
-const crowller = new Crowller();
+const secret = 'secretKey';
+const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
+
+const workingAnalyzer = new analyzer()
+new Crowller(url, workingAnalyzer);
  
 // const timer = setInterval(() => {
 //   // console.log(crowller)
