fd2f2991d8717daa2a87551f4797e9ebd07e4482
zerowch <zerowch@qq.com>
2020-02-24 11:04:47 +0800

    course4-1

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,59 +1,32 @@
-/*
- * @Author: Zero W
- * @Date: 2020-02-13 14:56:06
- * @Description: crowller
- * @LastEditTime : 2020-02-18 11:36:03
- * @LastEditors  : Zero W
- * @-<-zw->-
- */
-import superagent from 'superagent';
 import fs from 'fs';
 import path from 'path';
-import analyzer from './analyzer';
+import superagent from 'superagent';
 
 export interface Analyzer {
-  analyze: (html: string, filePath: string) => string
+  analyze: (html: string, filePath: string) => string;
 }
 
 class Crowller {
-  // private rawHtml = '';
-  // public crowData: unknown
-  private FilePath = path.resolve(__dirname, '../data/course.json')
+  private filePath = path.resolve(__dirname, '../data/course.json');
 
-  async getRawHtml () {
+  private async getRawHtml() {
     const result = await superagent.get(this.url);
     return result.text;
-    // this.getHtmlData(this.rawHtml)
   }
 
-  writeFile (content: string) {
-    const filePath = this.FilePath
-    fs.writeFileSync(filePath, content)
+  private writeFile(content: string) {
+    fs.writeFileSync(this.filePath, content);
   }
 
-  async initSpiderProcess () {
-    
-    const html = await this.getRawHtml()
-    const fileContent = this.analyzer.analyze(html, this.FilePath)
-    this.writeFile(fileContent)
+  private async initSpiderProcess() {
+    const html = await this.getRawHtml();
+    const fileContent = this.analyzer.analyze(html, this.filePath);
+    this.writeFile(fileContent);
   }
 
-  constructor (private url: string, private analyzer: Analyzer) {
-    console.log('crowller constructor');
+  constructor(private url: string, private analyzer: Analyzer) {
     this.initSpiderProcess();
   }
 }
 
-const secret = 'secretKey';
-const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`;
-
-const workingAnalyzer = new analyzer()
-new Crowller(url, workingAnalyzer);
- 
-// const timer = setInterval(() => {
-//   // console.log(crowller)
-//   console.log(crowller.crowData);
-//   if (crowller.crowData) {
-//     clearInterval(timer);
-//   }
-// }, 500);
+export default Crowller;
